17105ddf58aeaac09c0d9a51b5e5445b088e6e9f
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-01-16 22:13:22 +0100

    Add files via upload

---
+++ trailing_whitespaces.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+# Use standard Posix bracket Expression for removal:
+# [[:space:]] = all whitespace characters, including line breaks
+# [[:blank:]] = space and tab
+
+# Get the filename from the command line argument.
+FN=$1
+
+# Check if filename was given.
+if [ "${FN}" = '' ]; then
+    echo "No filename given. Bye!"
+    exit 1
+fi
+
+# Remove trailing whitespaces from file.
+sed -i 's/[[:space:]]*$//' "${FN}"
+
+# Exit script without error.
+exit 0
