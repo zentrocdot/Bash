2f33b22526a0e8f3c7e801ab85d6e44f611c56d7
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-01-24 13:52:58 +0100

    Update and rename trailing.bash to remove_nlcr_whitespaces.bash

--- file_tools/remove_nlcr_whitespaces.bash
+++ file_tools/remove_nlcr_whitespaces.bash
@@ -3,7 +3,7 @@
 # version 0.0.0.1
 #
 # Description:
-# Exchange newlines, carriage returns and tabs by one space and remove
+# Exchange l, carriage returns and tabs by one space and remove
 # leading and trailing spaces and
 #
 # Explanation:
