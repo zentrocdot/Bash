1082c17b33aa4175e1f0eaec6e7b3cf820246ef1
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-01-25 17:23:01 +0100

    Add files via upload

---
+++ file_tools/remove_leading_trailing_whitespaces.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+#
+# See also:
+# www.gnu.org/software/sed/manual/sed.html
+
+# Assign the command line argument to the global variable.
+FN=$1
+
+# Use sed for the replacements in the file.
+sed -i -e 's/^[[:space:]]\+//g;s/[[:space:]]\+$//g' "${FN}" 2> /dev/null
+
