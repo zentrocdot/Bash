829523820ef7902af6d4067a2232e91c34887f35
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-01-25 16:53:47 +0100

    Add files via upload

---
+++ file_tools/remove_empty_lines.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+#
+# See also:
+# www.gnu.org/software/sed/manual/sed.html
+
+# Assign the command line argument to the global variable.
+FN=$1
+
+# Remove comment lines and remove empty lines from file.
+sed -i -e '/^[[:space:]]*$/d' "${FN}" 2> /dev/null
+
