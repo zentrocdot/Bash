adaf079e683a11bc5e66c7a53c6fe32f77083502
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-01-26 21:59:06 +0100

    Add files via upload

--- file_tools/remove_comments.bash
+++ file_tools/remove_comments.bash
@@ -1,11 +1,62 @@
 #!/usr/bin/bash
 #
+# Remove Comments
+# Version 0.0.0.2
+# Copyright © 2024, Dr. Peter Netz
+# Published under the MIT license.
+#
+# You are using the script on your own risk. If you have any doubts whether
+# the script is working correctly, you should always make a backup copy of
+# the file to be on the secure side.
+#
+# Description:
+# The script removes all single-line and inline comments from a file that
+# start with a hashtag #. Inline comments must be preceded by a space before
+# the hashtag # for the inline comment to be valid as such. Regular comments
+# are recognised with or without indentation.
+#
+# Explanation:
+# First line is checked on the existence of a Shebang which is always
+# introduced by '#!/' followed by different possible paths. If there is a
+# Shebang, such a line is not a comment line. Regular comments are identified
+# with or without indentation and than deleted. Inline comments starts with
+# one or more space followed by a hashtag # with characters up to the end of
+# the line and removed.
+#
+# Functionality Check:
+# This script is used for the functionality check, based on the fact that in
+# the sed command there is the hashtag three times and this are not comments.
+#
+# Usage of hashtag # in Bash:
+#   # Comment lines
+#   ${#len_of_str}
+#   ${myvar#pattern}
+#   ${myvar##pattern}
+#
+# Limitation:
+# If a hashtag # is found between single or double quotes the inline comment
+# will not be touched.
+#
+# To-Do:
+# Check pattern matching of the inline comments and improve the recognition
+# if necessary.
+#
 # See also:
 # www.gnu.org/software/sed/manual/sed.html
+# www.gnu.org/software/sed/manual/html_node/Regular-Expressions.html
+# unix.stackexchange.com/questions/230351/how-to-correctly-use-quotes-in-sed
+# stackoverflow.com/questions/19482123/extract-part-of-a-string-using-bash-cut-split
+# www.asciitable.com/
 
 # Assign the command line argument to the global variable.
-FN=$1
+FN=$1 # comment for deletion
 
-# Remove comment lines from file.
-sed -i -e '/^[#]/d' "${FN}" 2> /dev/null
-
+# Remove comments from a file.
+# '0,/^#!\//p' -> Ignore line with Shebang
+# '/^[[:blank:]]*#/d' -> Delete regular comment lines
+# '/\x22.*#\+.*\x22/b' & '/\x27.*#\+.*\x27/b' -> Match hashtag # between quotes
+# 's/\(.*[[:space:]]#\).*$/\1/g;s/[[:space:]]\+[#]$//' -> Delete inline comments
+sed -i -e '0,/^#!\//p' -e '/^[[:blank:]]*#/d' \
+       -e '/\x22.*#\+.*\x22/b' -e '/\x27.*#\+.*\x27/b' \
+       -e 's/\(.*[[:space:]]\+#\).*$/\1/g;s/[[:space:]]\+[#]$//' \
+       "${FN}" # comment for deletion
