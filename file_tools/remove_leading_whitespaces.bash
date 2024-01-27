07f92ddf804abc1e20b57d1c138de5e6e159a119
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-01-27 13:03:47 +0100

    Add files via upload

---
+++ file_tools/remove_leading_whitespaces.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+#
+# [:space:]
+# tab, vertical tab, form feed, newline, line feed, carriage return and space
+# [:blank:]
+# tab and space
+#
+# See also:
+# www.gnu.org/software/sed/manual/sed.html
+
+# Assign the command line argument to the global variable.
+FN=$1
+
+# Use sed for the replacements in the file.
+sed -i 's/^[[:space:]]\+//g' "${FN}" 2> /dev/null
+
+# Exit script.
+exit 0
