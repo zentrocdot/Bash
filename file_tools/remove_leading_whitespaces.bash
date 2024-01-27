e92baa9069befde47675a5bbe282e3f4a453a697
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-01-27 13:07:58 +0100

    Add files via upload

--- file_tools/remove_leading_whitespaces.bash
+++ file_tools/remove_leading_whitespaces.bash
@@ -7,9 +7,16 @@
 #
 # See also:
 # www.gnu.org/software/sed/manual/sed.html
+# www.regular-expressions.info/posixbrackets.html
 
 # Assign the command line argument to the global variable.
 FN=$1
+
+# Check if filename was given.
+if [ "${FN}" = '' ]; then
+    echo "No filename given. Bye!"
+    exit 1
+fi
 
 # Use sed for the replacements in the file.
 sed -i 's/^[[:space:]]\+//g' "${FN}" 2> /dev/null
