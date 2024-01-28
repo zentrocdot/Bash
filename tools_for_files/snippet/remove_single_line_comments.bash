e0ea268201ec7720bdb5f5048e2ae9ac317da95d
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-01-28 14:52:53 +0100

    Update remove_single_line_comments.bash

--- tools_for_files/snippet/remove_single_line_comments.bash
+++ tools_for_files/snippet/remove_single_line_comments.bash
@@ -1,1 +1,10 @@
+#!/usr/bin/bash
 
+# Assign the command line argument to the global variable.
+FN=$1
+
+# Remove single line comments from file.
+sed -i '/^[[:blank:]]*#/d' "${FN}" 2> /dev/null
+
+# Exit script.
+exit 0
