9ccb43345433c7a137881d30d373ede7efdc79ba
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-01-28 14:37:51 +0100

    Add files via upload

---
+++ tools_for_files/remove_multiple_spaces.bash
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+#
+# Exchange Tab To Space
+# Version 0.0.0.1
+# Copyright © 2024, Dr. Peter Netz
+# Published under the MIT license.
+#
+# [:space:]
+# tab, vertical tab, form feed, newline, line feed, carriage return and space
+# [:blank:]
+# tab and space
+#
+# See also:
+# www.gnu.org/software/sed/manual/sed.html
+# www.regular-expressions.info/posixbrackets.html
+
+# Assign the command line argument to the global variable.
+FN=$1
+
+# Check if filename was given.
+if [ "${FN}" = '' ]; then
+    echo "No filename given. Bye!"
+    exit 1
+fi
+
+# Remove multiple spaces from file.
+sed -i 's/[[:blank:]]\+/ /g' "${FN}" 2> /dev/null
+
+# Exit script.
+exit 0
