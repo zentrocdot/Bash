b7a8607bbf3cbc2c2c22b4190d4bd15b006a823a
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-01-29 10:14:27 +0100

    Update count_lines.bash

--- tools_for_files/snippets/count_lines.bash
+++ tools_for_files/snippets/count_lines.bash
@@ -5,22 +5,22 @@ FN=$1
 
 # Count the lines in a file.
 sed -n '
-/^/! b done               
-x                         
-/^$/ s/^.*$/0/            
-/^9*$/ s/^/0/             
-s/.9*$/z&/               
-h                         
-s/^.*z//                  
-y/0123456789/1234567890/  
-x                        
-s/z.*$//                  
-G                        
-s/\n//                   
-h                         
-:done                     
-$ {x;p}                   
+/^/! b done
+x
+/^$/ s/^.*$/0/
+/^9*$/ s/^/0/
+s/.9*$/z&/
+h
+s/^.*z//
+y/0123456789/1234567890/
+x
+s/z.*$//
+G
+s/\n//
+h
+:done
+$ {x;p}
 ' "${FN}" 2> /dev/null
 
-# Exit the script with an exit code.
+# Exit the script.
 exit 0
