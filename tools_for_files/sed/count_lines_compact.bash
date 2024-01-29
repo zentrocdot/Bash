8a61cdc12562cea2a3b204eb2916b6d99c57fd8c
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-01-29 11:58:50 +0100

    Add files via upload

--- tools_for_files/sed/count_lines_compact.bash
+++ tools_for_files/sed/count_lines_compact.bash
@@ -1,14 +1,10 @@
 #!/usr/bin/bash
 #
-# Count Lines in file
-#
-# Acknowledgement:
-# stackoverflow.com/questions/1781329/count-the-number-of-occurrences-of-a-string-using-sed
+# Count the lines in a file
 
 FN=$1
 
-PATTERN=""
+sed -n '/^/! ba;x;/^$/ s/^.*$/0/;/^9*$/ s/^/0/;s/.9*$/z&/;h;s/^.*z//;
+        y/0123456789/1234567890/;x;s/z.*$//;G;s/\n//;h;:a;$ {x;p}' "${FN}"
 
-sed -n "/^${PATTERN}/! bl; x; /^$/ s/^.*$/0/; /^9*$/ s/^/0/;
-        s/.9*$/x&/; h; s/^.*x//; y/0123456789/1234567890/; x;
-        s/x.*$//; G; s/\n//; h; :l; $ {x;p}" "${FN}"
+exit 0
