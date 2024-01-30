508fe62af9cdd51f71df766b04f5b0836a9ca8e6
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-01-30 21:02:18 +0100

    Update ip_address.bash

--- ip_address.bash
+++ ip_address.bash
@@ -12,10 +12,4 @@ echo "${STR}" | sed -n -E 's/.*[[:blank:]](([0-9]{1,3}\.){3}[0-9]{1,3})[[:blank:
 
 echo "${STR}" | sed -n 's/.*[[:blank:]]\(\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}\)[[:blank:]].*/\1/p'
 
-STR="mac 28:EF:52:01:EB:84 address"
-
-echo "${STR}" | sed -n -E 's/.*[[:blank:]](([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}[[:blank:]]).*/\1/p'
-
-echo "${STR}" | sed -n 's/.*[[:blank:]]\(\([[:xdigit:]]\{2\}:\)\{5\}[[:xdigit:]]\{2\}[[:blank:]]\).*/\1/p'
-
 exit 0
