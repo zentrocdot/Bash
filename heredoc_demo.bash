836bdcda62897fd88107c91e3e30693c6df57469
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-01-31 11:59:42 +0100

    Add files via upload

---
+++ heredoc_demo.bash
@@ -0,0 +1,53 @@
+#!/usr/bin/bash
+#
+# See also:
+# www.gnu.org/software/bash/manual/html_node/Redirections.html#Here-Documents
+
+echo -e "\nVersion 1\n"
+
+# Lorem ipsum heredoc.
+IFS='' read -d '' -r HERESTR1 <<'HEREDOC'
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+sanctus est Lorem ipsum dolor sit amet.
+HEREDOC
+
+echo -en "${HERESTR1}"
+
+echo -e "\nVersion 1 with indent of 4 chars\n"
+
+# Lorem ipsum heredoc.
+IFS='' read -d '' -r HERESTR2 <<-'HEREDOC'
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+    eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+    voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+    clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+    amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+    rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+    ipsum dolor sit amet.
+HEREDOC
+
+echo -en "${HERESTR2}"
+
+echo -e "\nVersion 3\n"
+
+# Lorem ipsum heredoc.
+HERESTR3=$(
+cat <<'HEREDOC'
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+sanctus est Lorem ipsum dolor sit amet.
+HEREDOC
+)
