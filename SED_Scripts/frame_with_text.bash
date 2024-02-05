ea8996b1cabe8f312a8ed2d1cec5c82dce4bed02
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-02-05 17:04:33 +0100

    Add files via upload

--- SED_Scripts/frame_with_text.bash
+++ SED_Scripts/frame_with_text.bash
@@ -1,46 +1,42 @@
 #!/usr/bin/bash
 #
 # Print frame with text
-# Version 0.0.0.2
+# Version 0.0.0.1
 
 # Assign the command line argument to the global variable.
-# Odd number of characters.
-#STR=${1:-This String Is A Good Sample Text Example}
-# Even number of characters.
+# For Odd number of characters use:
+# STR=${1:-This String Is A Good Sample Text Example}
+# For even number of charactersuse:
 STR=${1:-This String Is A Good Sample Text Example!}
 
-# Next block can be used to test odd and even text lengths.
-# Exchange command line in the sed script to do this.
-# Get the lenght of the string.
-##LEN=${#STR}
-## Calculate the modulo.
-##N=$((LEN%2))
-## Add a depending on the modulo.
-##if [[ "${N}" = "0" ]]; then
-##    STR+=" "
-##fi
+# Get the length of the string.
+LEN=${#STR}
 
-# Set the char.
+# Set the char. Default is #.
 CHR=${2:-#}
 
-# Set the width.
-LINE_WIDTH="80"
+# Set the line width. Default is 80.
+LINE_WIDTH=${3:-80}
 
-# Set the width
-WIDTH=$((LINE_WIDTH-2))
+# Check the given line width.
+if [[ "${LINE_WIDTH}" -lt "${LEN}" ]]; then
+    LINE_WIDTH=$((LEN+3))
+fi
+
+# Set the inner width.
+WIDTH=$((LINE_WIDTH-3))
 
 # Run the sed script.
-sed -n "
+sed -n '
 # Print the head line.
-x;s/^.*$/${CHR}/;:x;/^.\{1,${LINE_WIDTH}\}$/ {s/^\(.*\)$/${CHR}\1/;bx;};p
+x;s/^.*$/'"${CHR}"'/;:y0;/^.\{1,'"${LINE_WIDTH}"'\}$/ {s/^\(.*\)$/'"${CHR}"'\1/;by0;};p
 # Print the text
-## The next command line is sensitive to even and odd text lengths.
-##x;:y;/^.\{1,${WIDTH}\}$/ {s/^\(.*\)$/\x20\1\x20/;by};s/^/${CHR}/;s/$/${CHR}/;p
-x;:y;/^.\{1,${WIDTH}\}$/ {s/^\(.*\)$/\x20\1\x20/;by};s/\(..\{1,${WIDTH}\}\).*/\1/;s/^/${CHR}/;s/$/${CHR}/;p
+x;:y;/^.\{1,'"${WIDTH}"'\}$/ {s/^\(.*\)$/ \1\ /;by};s/\(..\{1,'"${WIDTH}"'\}\).*/\1/;
+s/^/'"${CHR}"'/;s/$/ '"${CHR}"'/;p
 # Print the foot line.
-x;s/^.*$/${CHR}/;:z;/^.\{1,${LINE_WIDTH}\}$/ {s/^\(.*\)$/${CHR}\1/;bz;};p
+x;s/^.*$/'"${CHR}"'/;:z0;/^.\{1,'"${LINE_WIDTH}"'\}$/ {s/^\(.*\)$/'"${CHR}"'\1/;bz0;};p
 # We are done.
-" < <(echo "${STR}") 2> /dev/null
+' < <(echo "${STR}") 2> /dev/null
 
 # Exit the script.
 exit 0
