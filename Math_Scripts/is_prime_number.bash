9c533ac7533b5716e5f0c4f878d6d6c3a7fe31ce
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-02-13 11:55:25 +0100

    Add files via upload

--- Math_Scripts/is_prime_number.bash
+++ Math_Scripts/is_prime_number.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #
 # Is prime number
-# Version 0.0.0.1
+# Version 0.0.0.2
 #
 # Description:
 # Application of a simple approach to check if a given number is a prime
@@ -12,9 +12,11 @@
 # After that writing a more sophisticated script for the check.
 #
 # See also:
+# www.gnu.org/software/bash/manual/bash.html
 # www.baeldung.com/linux/bash-prime-number-test
 # www.wikihow.com/Check-if-a-Number-Is-Prime
 # www.rookieslab.com/posts/fastest-way-to-check-if-a-number-is-prime-or-not
+# stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash
 
 # Assign the command line argument to the global variable.
 NUM=$1
@@ -29,12 +31,14 @@ is_prime_number () {
     # Assign the function argument to the local variable.
     num=$1
     # Initialise the variable is_prime.
-    is_prime=-1
+    is_prime=0
     # Check if a valid number is given.
+    # shellcheck disable=SC2292
     [ -n "${num}" ] && [ "${num}" -eq "${num}" ] 2>/dev/null
+    # shellcheck disable=SC2181
     if [[ $? -ne 0 ]] || [[ "${num}" -lt 1 ]]; then
-        echo -n "42"
-        return 42
+        echo -n "2"
+        return 2
     fi
     # Loop over the range of meaningful divisors.
     for((i=2; i<=((num/2)); i++))
@@ -42,12 +46,11 @@ is_prime_number () {
         # Calculate the remainder of the division.
         rem=$((num%i))
         # Change the value of is_prime if it is not a prime number.
-        if [ "${rem}" -eq 0 ]; then
+        if [[ "${rem}" -eq 0 ]]; then
             is_prime=1
             break
         fi
     done
-    is_prime=0
     # Output 0 if it is a prime number otherwise 1.
     echo -n "${is_prime}"
     # Return 0.
@@ -58,11 +61,11 @@ is_prime_number () {
 is_prime=$(is_prime_number "${NUM}")
 
 # Return the result of the check.
-if [[ "${is_prime}" -eq 1 ]]; then
-    echo "${NUM} is NOT a prime number."
-elif [[ "${is_prime}" -eq 0 ]]; then
+if [[ "${is_prime}" -eq 0 ]]; then
     echo "${NUM} is a prime number."
-elif [[ "${is_prime}" -eq 42 ]]; then
+elif [[ "${is_prime}" -eq 1 ]]; then
+    echo "${NUM} is NOT a prime number."
+elif [[ "${is_prime}" -eq 2 ]]; then
     echo "${NUM} is NOT a valid number."
 else
     echo "Unknown state of function."
