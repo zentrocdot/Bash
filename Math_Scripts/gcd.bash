1df95f32f1aac239c3d2e5470e5823f039d6bbfe
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-02-15 16:45:43 +0100

    Add files via upload

--- Math_Scripts/gcd.bash
+++ Math_Scripts/gcd.bash
@@ -1,41 +1,49 @@
 #!/usr/bin/bash
+#
+# Calculate the greates common divisor
+# Version 0.0.0.2
+# Copyright © 2024, Dr. Peter Netz
+# Published under the MIT license.
+# github.com/zentrocdot/Bash_Programming/tree/main?tab=MIT-1-ov-file
+#
+# See also:
+# de.wikipedia.org/wiki/Bildauflösungen_in_der_Digitalfotografie
 
-#a=2582
-#b=1549
+# Initialise the values.
+a=${1:-5184}
+b=${2:-3456}
 
-a=5184
-b=3456
-
-function gcd() {
-    local x=$1
-    local y=$2
-    while [ $y -gt 0 ]; do
-        local t=$x
-        x=$y
-        y=$((t%y))
+# ############
+# Function gcd
+# ############
+function gcd () {
+    # Assign the function arguments to the local variables.
+    local a=$1
+    local b=$2
+    # Declare the variable n.
+    local n
+    # Loop until the gcd is found.
+    while [[ "${b}" -gt 0 ]]
+    do
+        n="${a}"
+        a="${b}"
+        b=$((n%b))
     done
-    echo $x
+    # Output the greatest common devisor.
+    echo "${a}"
+    # Return 0.
+    return 0
 }
 
+# +++++++++++++++++++
+# Main script section
+# +++++++++++++++++++
 
+# Call the function:
+GCD=$(gcd "${a}" "${b}")
 
-gcd $a $b
+# Print the gcd.
+echo -e "${GCD}"
 
-exit 1
-
-m=$a
-if [ $b -lt $m ]
-then
-m=$b
-fi
-while [ $m -ne 0 ]
-do
-x=`expr $a % $m`
-y=`expr $b % $m`
-if [ $x -eq 0 -a $y -eq 0 ]
-then
-echo gcd of $a and $b is $m
-break
-fi
-m=`expr $m - 1`
-done
+# Exit the script.
+exit 0
