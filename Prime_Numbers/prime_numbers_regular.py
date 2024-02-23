e19b41be3675261e24990df0ab2b47b53b9c5ffc
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-02-23 11:59:02 +0100

    Update prime_numbers_regular.py

--- Prime_Numbers/prime_numbers_regular.py
+++ Prime_Numbers/prime_numbers_regular.py
@@ -3,7 +3,7 @@
 # pylint: disable=useless-return
 #
 # Calculate prime numbers
-# CPU version
+# Regular version
 # Version 0.0.0.1
 # Copyright © 2024, Dr. Peter Netz
 # Published under the MIT license.
